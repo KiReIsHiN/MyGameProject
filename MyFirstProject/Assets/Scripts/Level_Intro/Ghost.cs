2830bf203187c471cad7ffd50584550eefc5ca14
KiReIsHiN <kireishinalex@gmail.com>
2021-08-10 12:20:22 +0300

    Обновление до финальной версии
    Добавлено:
    Урок 8
    Настроить Quality Settings.
    Урок 7
    1. Настроить Сглаживание, Ambient Occlusion, Motion Blur, Bloom, Screen Space Reflection.
    2. Реализовать смену эффектов (при перемещении игрока в триггер или по нажатию на кнопку в интерфейсе).
    Урок 6
    Подобрать оптимальный тип Forward или Deferred rendering, который позволит вам иметь наименьшее количество draw calls для вашей сцены. Напишите в тексте ДЗ, какой вы выбрали и почему.
    4. ...и сделать металлические материалы на сцене (добавлено одно зеркало). *

--- MyFirstProject/Assets/Scripts/Level_Intro/Ghost.cs
+++ MyFirstProject/Assets/Scripts/Level_Intro/Ghost.cs
@@ -1,17 +1,51 @@
-
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
-    public GameObject _ghost;
-    
+    [SerializeField]
+    private GameObject _ghost;
+
+    private PostProcessVolume _postProcessVolume;
+    private Vignette _vignette;
+
+    private void Start()
+    {
+        SettingsVignette();
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
 
-            Destroy(_ghost);  
+            Destroy(_ghost);
+
+            Destroy(this.gameObject, 10);
+
+            _postProcessVolume = PostProcessManager.instance.QuickVolume(7, 2, _vignette);
         }
+    }
+
+    private void Update()
+    {
+        _vignette.intensity.value = Mathf.Sin(Time.realtimeSinceStartup);
+    }
+
+
+    private void SettingsVignette()
+    {
+        _vignette = ScriptableObject.CreateInstance<Vignette>();
+        _vignette.enabled.Override(true);
+        _vignette.intensity.Override(1);
+        _vignette.smoothness.Override(1);
+        _vignette.roundness.Override(1);
+    }
+
+    
+
+    private void OnDestroy()
+    {
+        RuntimeUtilities.DestroyVolume(_postProcessVolume, true, true);
     }
 }
